a666c2794db0a30e80601d71022cbd5c7768597b
ChDarina <idkkunmaybechan@gmail.com>
2021-04-29 23:01:49 +0500

    Добавьте файлы проекта.

---
+++ NotebookAPI/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotebookAPI.Models
+{
+    public class Contact : BaseEntity
+    {
+        public string Value { get; set; }
+
+        public int? PersonId { get; set; }
+
+        [JsonIgnore]
+        public Person Person { get; set; }
+
+        public int? ContactTypeId { get; set; }
+        public ContactType ContactType { get; set; }
+    }
+
+}
